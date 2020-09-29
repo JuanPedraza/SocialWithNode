27f0924b9937eda615982ac19c7096b73e626e79
Juan Pedraza <juan_pedraza@outlook.com>
2020-09-29 17:38:34 -0500

    Second Commit

--- store/dummy.js
+++ store/dummy.js
@@ -1,24 +1,24 @@
 const db = {
     'user':[
         {
-            id: 1,
+            id: "1",
             name: 'Marcus Fenix'
         },
         
     ]
 }
 
-function list(table){
+async function list(table){
     return db[table]
 }
-function get(table, id){
-    let col = list(table)
+async function get(table, id){
+    let col = await list(table)
     return col.filter( item => item.id === id) [0] || null
 }
-function upsert(table, data){
+async function upsert(table, data){
     db[collection].push(data)
 }
-function remove(id,table){
+async function remove(id,table){
     return true
 }
 
